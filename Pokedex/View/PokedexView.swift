90ba41e6102664839b07fad292d2586924ad707e
Reginaldo Morikawa <morikawa77@gmail.com>
2021-02-23 02:21:34 -0300

    First commit

---
+++ Pokedex/View/PokedexView.swift
@@ -0,0 +1,26 @@
+//
+//  PokedexView.swift
+//  Pokedex
+//
+//  Created by Reginaldo Morikawa on 23/02/21.
+//
+
+import SwiftUI
+
+struct PokedexView: View {
+    private let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
+    @ObservedObject var viewModel = PokemonViewModel()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: gridItems, spacing: 16) {
+                    ForEach(viewModel.pokemon) { pokemon in
+                        PokemonCell(pokemon: pokemon, viewModel: viewModel)
+                    }
+                }
+            }
+            .navigationTitle("Pokedex")
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Pokedex
+
+<p align="center">
+ <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=15C3D6&labelColor=000000">
+</p>
+
+<br>
+
+<p align="right">
+  <img alt="Pokedex" src="screenshot.jpeg">
+</p>
+
+## 💻 Projeto
+
+Pokedex feito com SwiftUI
+
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+---
+
+Feito com ♥ by morikawa77
